ff19a0aba75b3d7660f7d2a865e9bb3da66dc5e8
Michal Ciurej <67523245+michal-ciurej@users.noreply.github.com>
2023-05-18 21:25:20 +0100

    added chart

---
+++ script.js
@@ -0,0 +1,35 @@
+function data(
+    
+data = fetch("https://ontario.udmi.io/api/sensor/1/readings")
+.then((response) => response.json()) 
+
+
+
+)
+
+
+function setChart(data){
+    const obj = JSON.parse(data)
+    for (var i =0; i< obj.length ;i++) {
+        console.log(obj[i].id);
+     }
+    }
+
+
+
+console.log(chart1.readings)
+
+
+AFRAME.registerComponent('markerhandler', {
+    init: function () {
+      this.el.sceneEl.addEventListener('markerFound', () => {
+        console.log("Caught Marker external");
+    
+       
+              })
+      this.el.sceneEl.addEventListener('markerLost', () => {
+        console.log("Lost Marker external");
+    
+      })
+    }
+    });

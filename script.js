c753f9189da4f6fe8180c20ccc0b5017beed4f8b
Michal Ciurej <67523245+michal-ciurej@users.noreply.github.com>
2023-05-18 22:04:38 +0100

    Works

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-function data(
-    
-data = fetch("https://ontario.udmi.io/api/sensor/1/readings")
-.then((response) => response.json()) 
-
-)
-
-
-
-
-
-function setChart(data){
-
-
-    const obj = JSON.parse(data)
-    for (var i =0; i< obj.length ;i++) {
-        console.log(obj[i].id);
-     }
-    }
-
-console.log(chart1.readings)
-
 AFRAME.registerComponent('markerhandler', {
     init: function () {
       this.el.sceneEl.addEventListener('markerFound', () => {
         console.log("Caught Marker external");
+
         document.getElementById("myChart").style.visibility = "visible";
 
     
@@ -31,9 +10,21 @@ AFRAME.registerComponent('markerhandler', {
               })
       this.el.sceneEl.addEventListener('markerLost', () => {
         console.log("Lost Marker external");
+        
         document.getElementById("myChart").style.visibility = "hidden";
 
     
       })
     }
     });
+
+function data(
+    
+data = fetch("https://ontario.udmi.io/api/sensor/1/readings")
+.then((response) => response.json()) 
+
+){};
+
+
+
+
